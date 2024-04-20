42493e2d6c7a3053751bafdf1f4fffbeca5e1207
Alexander Naumov <anaumov@solutionmentors.com>
2024-04-20 19:10:20 +0300

    extract flip logic to class.

--- js/app.js
+++ js/app.js
@@ -3,16 +3,60 @@ const hours = currentTime.getHours();
 const minutes = currentTime.getMinutes();
 const seconds = currentTime.getSeconds();
 console.log(`${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`);
-let n = seconds % 10;
 
-let isPage1 = true;
-
-let secPairCards = document.querySelector('#seconds');
 let x = document.querySelector('#ss-up');
+let xx = document.querySelector('#s-up');
+let secPairCards = document.querySelector('#seconds');
 let sec = document.querySelector("#o-s");
+let secC = document.querySelector("#s-o");
 
-const nextNum = (n) => {
-    return n + 1 > 9 ? 0 : n + 1;
+class FlipCard {
+    constructor(element, page1, page2, maxN = 9) {
+        this.element = element;
+        this.isPage1 = true;
+        this.page1 = page1;
+        this.page2 = page2;
+        this.n = 0;
+        this.maxN = maxN;
+        this.upValue = this.element.querySelector('.up .v');
+        this.lowValue = this.element.querySelector('.low .v');
+        this.backValue = this.element.querySelector('.back .v');
+        this.upCard = this.element.querySelector('.up');
+    }
+
+    flip () {
+        this.lowValue.innerHTML = Number.parseInt(this.upValue.innerHTML);
+        if (this.isPage1) {
+            this.upCard.classList.remove(this.page1);
+            this.upValue.style.display = 'block';
+            this.upValue.style.transform = 'translateY(-50%)';
+            this.upValue.style.transform = 'rotateX(-180deg)';
+            this.upCard.classList.add(this.page2);
+            this.isPage1 = false;
+            this.n = Number.parseInt(this.upValue.innerHTML);
+            this.upValue.innerHTML = nextNum(this.n, this.maxN);
+            this.lowValue.innerHTML = this.n;
+        } else {
+            this.upCard.classList.remove(this.page2);
+            this.upValue.style.transform = 'translateY(0%)';
+            this.upValue.style.transform = 'rotateX(0deg)';
+            this.upCard.classList.add(this.page1);
+            this.isPage1 = true;
+            const backVal = Number.parseInt(this.upValue.innerHTML);
+            this.backValue.innerHTML = nextNum(backVal, this.maxN);
+        }
+    }
+
+    singleFlipPostActions() {
+        this.upCard.classList.remove(this.page1, this.page2);
+        this.upValue.style.transform = 'translateY(0%)';
+        this.upValue.style.transform = 'rotateX(0deg)';
+        this.lowValue.innerHTML = nextNum(this.n, this.maxN);
+    }
+}
+
+const nextNum = (n, maxN = 9) => {
+    return n + 1 > maxN ? 0 : n + 1;
 }
 
 const initCard = (card, n) => {
@@ -24,44 +68,42 @@ const initCard = (card, n) => {
     backValue.innerHTML = nextNum(n);
 }
 
-const initPairCards =(pairCards, num) => {
+const initPairCards = (pairCards, numValue) => {
     const rCard = pairCards.querySelector('[id^="o-"]');
-    initCard(rCard, num % 10);
+    initCard(rCard, numValue % 10);
     const lCard = pairCards.querySelector('[id$="-o"]');
-    initCard(lCard, Math.floor(num / 10));
+    initCard(lCard, Math.floor(numValue / 10));
 }
 
-const flip = (element) => {
-    let upValue = element.querySelector('.up .v');
-    let lowValue = element.querySelector('.low .v');
-    let backValue = element.querySelector('.back .v');
-    lowValue.innerHTML = nextNum(n);
-    if (isPage1) {
-        x.classList.remove('page1');
-        upValue.style.display = 'block';
-        upValue.style.transform = 'translateY(-50%)';
-        upValue.style.transform = 'rotateX(-180deg)';
-        x.classList.add('page2');
-        isPage1 = false;
-        n = Number.parseInt(upValue.innerHTML);
-        upValue.innerHTML = nextNum(n);
-        lowValue.innerHTML = n;
-    } else {
-        x.classList.remove('page2');
-        upValue.style.transform = 'translateY(0%)';
-        upValue.style.transform = 'rotateX(0deg)';
-        x.classList.add('page1');
-        isPage1 = true;
-        backValue.innerHTML = nextNum(n + 1);
-    }
-}
-
+/**
+ * initialization.
+ */
 initPairCards(secPairCards, seconds);
+const secFlipCard = new FlipCard(sec, 'page1', 'page2');
+const seccFlipCard = new FlipCard(secC, 'page11', 'page22', 5);
+seccFlipCard.isPage1 = false;
 
 x.addEventListener('click', () => {
     x.classList.add('page1');
 });
 
+xx.addEventListener('click', () => {
+    xx.classList.add('page11');
+});
+
+
 x.addEventListener('animationiteration', () => {
-    flip(sec);
+    secFlipCard.flip();
+    if (secFlipCard.isPage1 && secFlipCard.n + 1 === 9) {
+        console.log('1');
+        seccFlipCard.flip();
+    }
+});
+
+xx.addEventListener('animationend', () => {
+    if (seccFlipCard.isPage1) {
+        seccFlipCard.flip();
+    } else {
+        seccFlipCard.singleFlipPostActions();
+    }
 });
